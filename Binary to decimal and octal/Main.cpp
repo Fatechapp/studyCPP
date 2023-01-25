6151380b49465f46d876284acc99241dbc715c02
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2023-01-25 22:17:27 +0700

    Update Main.cpp

--- Binary to decimal and octal/Main.cpp
+++ Binary to decimal and octal/Main.cpp
@@ -13,17 +13,29 @@ int binaryToDecimal(long long n) {
     }
     return decimalNumber;
 }
+int binaryToOctal(long long binaryNumber)
+{
+    int octalNumber = 0, decimalNumber = 0, i = 0;
 
-int binaryToOctal(long long n) {
-    int octalNumber = 0, i = 1, remainder;
-    while (n!=0) {
-        remainder = n%1000;
-        n /= 1000;
-        octalNumber += remainder*i;
+    while(binaryNumber != 0)
+    {
+        decimalNumber += (binaryNumber%10) * pow(2,i);
+        ++i;
+        binaryNumber/=10;
+    }
+
+    i = 1;
+
+    while (decimalNumber != 0)
+    {
+        octalNumber += (decimalNumber % 8) * i;
+        decimalNumber /= 8;
         i *= 10;
     }
+
     return octalNumber;
 }
+
 
 int main() {
     long long binaryNum;
